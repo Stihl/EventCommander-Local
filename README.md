caed3f3770f0ee8cc74d30a9423871d605584483
Alex Gregg <agregg444@gmail.com>
2024-07-02 07:34:36 -0500

    Updated JS to handle occasion adding and saving.

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ Event Commander Local
 ---------------------
 Purpose:
   Event Commander Local is designed to allow a task display with data stored locally. to be used on a personal bulletin board for a monitor or kitchen display.
+
+Usage:
+  Simply enough Type into the fields at the bottom to add an event, use the date picker to choose a date or one will be given to you.
+  Each event will have an update button that in the future will pop up a modal to allow updating.
+  Each event already has a delete button that allows you to delete the event.
+
+Local Storage:
+  This version of Event Commander is designed specifically to use local storage and be run soley from your browser.
+
--- app.js
+++ app.js
@@ -1,16 +1,55 @@
-const { createApp, ref } = Vue
-import Occasion from "./occasion.js"
+const { createApp, ref, onMounted } = Vue
 
-const app = createApp({
+class Occasion {
+            
+        
+    constructor(title="", date=undefined, description="") {
+        this.title = title
+        this.date = date || new Date(Date.now())
+        this.description = description
+        this.passed = false
+    }
+
+    checkPassed() {
+        if(this.date < Date.now()) {
+            this.passed = true
+        } else {
+            this.passed = false
+        }
+    }
+//Next is the modal for editing and deleting
+createApp({
     setup() {
-        const occasions = ref([])
+        
+        const occasions = ref([new Occasion("title", "date", "description")])
+        const title = ref("")
+        const date = ref("")
+        const description = ref("")
+        
+        function saveAll() {
+            let storage = window.localStorage
+            storage.setItem("occasions", JSON.stringify(occasions.value));
+        }
+        function loadAll() {
+            let storage = window.localStorage
+            
+            occasions.value = JSON.parse(storage.getItem("occasions")) || []
+              
+        }
+        loadAll()
 
         function addOccasion() {
-            occasions.value.push(new Occasion("Birthday", Date.now(), "A birthday party!"));
+            
+            occasions.value.push(new Occasion(title.value, date.value, description.value));
+            saveAll()
+            title.value = ""
+            date.value = undefined
+            description.value = ""
         }
 
         function removeOccasion(index) {
             occasions.value.splice(index,1)
+            saveAll();
         }
 
         function updateOccasion(index) {
@@ -21,13 +60,17 @@ const app = createApp({
                     occasions.value[i].description = "Updated Description"
                 }
             }
+            saveAll()
         }
 
         return {
             occasions,
             addOccasion,
             removeOccasion,
-            updateOccasion
+            updateOccasion,
+            title,
+            date,
+            description
         }
     }
 
